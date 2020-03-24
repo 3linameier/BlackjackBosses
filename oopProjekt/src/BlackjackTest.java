14260a82969a05a1297fa0c57ac163028cc85139
Elina Meier <3linameier@gmail.com>
2020-03-24 23:27:30 +0200

    Kaart klassi täiendused ja test klass

---
+++ oopProjekt/src/BlackjackTest.java
@@ -0,0 +1,12 @@
+public class BlackjackTest {
+    public static void main(String[] args) {
+        Kaart potiKuus = new Kaart(6, 3);
+
+        System.out.println(potiKuus);
+    }
+}
+
+
+// Et oleks parem tegeleda nende kaartidega siis teeme süsteemi nii, et numbrid on nagu ikka ja poiss on 11, emand on 12 ja kunn on 13
+// Mastide jaoks paneme ka numbrid, siis saab nagu võrrelda vni. Ärtu on 0, ruutu on 1, risti on 2, poti on 3
+
--- oopProjekt/src/Kaart.java
+++ oopProjekt/src/Kaart.java
@@ -1,5 +1,9 @@
 public class Kaart {
 
+    public static final String[] numbrid = new String[]{null, "äss", "kaks", "kolm", "neli", "viis", "kuus", "seitse", "kaheksa",
+            "üheksa", "kümme", "poiss", "emand", "kuningas"};
+    public static final String[] mastid = new String[]{"Ärtu", "Ruutu", "Risti", "Poti"};
+
     private int number;
     private int mast;
 
@@ -14,5 +18,11 @@ public class Kaart {
     public Kaart(int number, int mast) {
         this.number = number;
         this.mast = mast;
+    }
+
+
+    @Override
+    public String toString() {
+        return  mastid[this.mast] + " " + numbrid[this.number];
     }
 }
