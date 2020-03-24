ae0d9a481f94645ff6f6c6f1864da496eaf4aae3
Elina Meier <3linameier@gmail.com>
2020-03-24 23:08:27 +0200

    Initial commit

---
+++ oopProjekt/src/Kaart.java
@@ -0,0 +1,18 @@
+public class Kaart {
+
+    private int number;
+    private int mast;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getMast() {
+        return mast;
+    }
+
+    public Kaart(int number, int mast) {
+        this.number = number;
+        this.mast = mast;
+    }
+}
